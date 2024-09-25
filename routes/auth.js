c31c22b883839976c8bf8d8721d31e2c95d2a37e
Nouaamane <norou6@gmail.com>
2024-09-25 22:40:31 +0100

    add login endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const asyncHandler = require('express-async-handler')
 const {User, validateUserRegister,validateUserLogin, validateUserUpdate} = require("../models/User");
-
+const bcrypt = require('bcryptjs');
 /**
  * @desc Register New User
  * @route /api/auth/register
@@ -19,6 +19,9 @@ router.post('/register', asyncHandler(async (req,res)=>{
     if (user){
         return res.status(400).json ({message : "THis user is already registered "})
     }
+    const salt = await bcrypt.genSalt(10)
+    req.body.password = await bcrypt.hash(req.body.password, salt)
+
     user = new User({
         email : req.body.email,
         username : req.body.username,
@@ -26,7 +29,35 @@ router.post('/register', asyncHandler(async (req,res)=>{
         isAdmin : req.body.isAdmin
     });
     const result = await user.save();
-    res.status(201).json(result);
+    const token = null;
+    const {password, ...other} = result._doc;
+    res.status(201).json({...other, token});
+}));
+
+/**
+ * @desc Login to Account
+ * @route /api/auth/Login
+ * @method POST
+ * @access public
+ */
+
+router.post('/login', asyncHandler(async (req,res)=>{
+    const {error} = validateUserLogin(req.body);
+    if(error){
+        return res.status(400).json({ message: error.details[0].message })
+    }
+    let user = await User.findOne({ email: req.body.email });
+    if (!user){
+        return res.status(400).json ({message : "invalid Email !"})
+    }
+    const isPasswordMatch = await  bcrypt.compare(req.body.password, user.password);
+    if (!isPasswordMatch){
+        return res.status(400).json ({message : "invalid Password !"})
+    }
+
+    const token = null;
+    const {password, ...other} = user._doc;
+    res.status(201).json({...other, token});
 }));
 
 module.exports = router;
