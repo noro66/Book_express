0772beaed83d987b2c1888caf96ec4d7d54dab74
Nouaamane <norou6@gmail.com>
2024-09-25 14:29:07 +0100

    add costume middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const booksPath = require('./routes/books')
 const authorsPath = require('./routes/authors')
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
+const logger = require("./middlewares/logger");
 dotenv.config();
 
 
@@ -12,9 +13,13 @@ const app = express();
 mongoose.connect(process.env.MONGO_URL)
     .then(()=> console.log("connected  To Mongodb..."))
     .catch(error => console.log("connection failed  To MongoDB", error))
+
+//costume middleware
+app.use(logger);
 //Routes
 app.use("/api/books", booksPath);
 app.use("/api/authors", authorsPath);
+
 
 
 //lunching the server
